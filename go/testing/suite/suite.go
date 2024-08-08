1ee423bbc243c6a7021bea4724489df34c83c2ac
Patrick Lee <pattyshack101@gmail.com>
2024-08-07 20:00:55 -0700

    improve error msg

--- go/testing/suite/suite.go
+++ go/testing/suite/suite.go
@@ -1,262 +1,275 @@
 package suite
 
 import (
-  "reflect"
-  "strings"
-  "testing"
+	"reflect"
+	"strings"
+	"testing"
 )
 
 var (
-  // lower case -> camel case
-  specialFuncNames = map[string]string {
-    "setupsuite": "SetupSuite",
-    "setuptest": "SetupTest",
-    "setupbenchmark": "SetupBenchmark",
-    "cleanupsuite": "CleanupSuite",
-    "cleanuptest": "CleanupTest",
-    "cleanupbenchmark": "CleanupBenchmark",
-  }
+	// lower case -> camel case
+	specialFuncNames = map[string]string{
+		"setupsuite":       "SetupSuite",
+		"setuptest":        "SetupTest",
+		"setupbenchmark":   "SetupBenchmark",
+		"cleanupsuite":     "CleanupSuite",
+		"cleanuptest":      "CleanupTest",
+		"cleanupbenchmark": "CleanupBenchmark",
+	}
 )
 
 // If this interface is defined for the suite, then RunTests/RunBenchmarks
 // will call this setup method before any of the suite's tests/benchmarks.
 // This is called once per RunTests/RunBenchmarks.
 type SuiteSetup interface {
-  // CAUTION: This expects testing.TB rather than *testing.T or *testing.B.
-  SetupSuite(tb testing.TB)
+	// CAUTION: This expects testing.TB rather than *testing.T or *testing.B.
+	SetupSuite(tb testing.TB)
 }
 
 // If this interface is defined for the suite, then RunTests/RunBenchmarks
 // will call this cleanup method after tests/benchmark completed.  This is
 // called once per RunTests/RunBenchmarks.
 type SuiteCleanup interface {
-  // CAUTION: This expects testing.TB rather than *testing.T or *testing.B.
-  CleanupSuite(tb testing.TB)
+	// CAUTION: This expects testing.TB rather than *testing.T or *testing.B.
+	CleanupSuite(tb testing.TB)
 }
 
 // If this interface is defined for the suite, then RunTests will call this
 // setup method before each test in the suite.
 type TestSetup interface {
-  SetupTest(t *testing.T)
+	SetupTest(t *testing.T)
 }
 
 // If this interface is defined for the suite, then RunTests will call this
 // cleanup method after each test in the suite.
 type TestCleanup interface {
-  CleanupTest(t *testing.T)
+	CleanupTest(t *testing.T)
 }
 
 // If this interface is defined for the suite, then RunBenchmarks will call
 // this setup method before each benchmark test run in the suite.
 type BenchmarkSetup interface {
-  SetupBenchmark(b *testing.B)
+	SetupBenchmark(b *testing.B)
 }
 
 // If this interface is defined for the suite, then RunBenchmarks will call
 // this cleanup method after each benchmark test run in the suite.
 type BenchmarkCleanup interface {
-  CleanupBenchmark(b *testing.B)
+	CleanupBenchmark(b *testing.B)
 }
 
 func checkSuiteSignatures[SuiteT any](tb testing.TB, suite SuiteT) {
-  tb.Helper()
+	tb.Helper()
 
-  suiteValue := reflect.ValueOf(suite)
-  suiteType := reflect.TypeOf(suite)
+	suiteValue := reflect.ValueOf(suite)
+	suiteType := reflect.TypeOf(suite)
 
-  for i := 0; i < suiteValue.NumMethod(); i++ {
-    methodName := suiteType.Method(i).Name
+	for i := 0; i < suiteValue.NumMethod(); i++ {
+		methodName := suiteType.Method(i).Name
 
-    // suiteValue.Method instead of suiteType.Method to get the receiver-binded
-    // method.
-    methodValue := suiteValue.Method(i)
+		// suiteValue.Method instead of suiteType.Method to get the receiver-binded
+		// method.
+		methodValue := suiteValue.Method(i)
 
-    if strings.HasPrefix(methodName, "Test") {
-      _, ok := methodValue.Interface().(func(*testing.T))
-      if !ok {
-        tb.Fatalf("Found unexpected method signature for %s", methodName)
-      }
+		if strings.HasPrefix(methodName, "Test") {
+			_, ok := methodValue.Interface().(func(*testing.T))
+			if !ok {
+				tb.Fatalf(
+					"Unexpected method signature for %s. Expected func(*testing.T)",
+					methodName)
+			}
 
-    } else if strings.HasPrefix(methodName, "Benchmark") {
-      _, ok := methodValue.Interface().(func(*testing.B))
-      if !ok {
-        tb.Fatalf("Found unexpected method signature for %s", methodName)
-      }
+		} else if strings.HasPrefix(methodName, "Benchmark") {
+			_, ok := methodValue.Interface().(func(*testing.B))
+			if !ok {
+				tb.Fatalf(
+					"Unexpected method signature for %s. Expected func(*testing.B)",
+					methodName)
+			}
 
-    } else {
-      lowerMethodName := strings.ToLower(methodName)
+		} else {
+			lowerMethodName := strings.ToLower(methodName)
 
-      specialFuncName, ok := specialFuncNames[lowerMethodName]
-      if !ok {
-        continue
-      }
+			specialFuncName, ok := specialFuncNames[lowerMethodName]
+			if !ok {
+				continue
+			}
 
-      if methodName != specialFuncName {
-        // This is probably a typo.
-        tb.Errorf(
-          "Found %s method defined for suite (expected %s)",
-          methodName,
-          specialFuncName)
-      }
+			if methodName != specialFuncName {
+				// This is probably a typo.
+				tb.Errorf(
+					"Found %s method defined for suite (expected %s)",
+					methodName,
+					specialFuncName)
+			}
 
-      if strings.HasSuffix(methodName, "Suite") {
-        _, ok := methodValue.Interface().(func(testing.TB))
-        if !ok {
-          tb.Fatalf("Found unexpected method signature for %s", methodName)
-        }
+			if strings.HasSuffix(methodName, "Suite") {
+				_, ok := methodValue.Interface().(func(testing.TB))
+				if !ok {
+					tb.Fatalf(
+						"Unexpected method signature for %s. Expected func(testing.TB)",
+						methodName)
+				}
 
-      } else if strings.HasSuffix(methodName, "Test") {
-        _, ok := methodValue.Interface().(func(*testing.T))
-        if !ok {
-          tb.Fatalf("Found unexpected method signature for %s", methodName)
-        }
-      } else if strings.HasSuffix(methodName, "Benchmark") {
-        _, ok := methodValue.Interface().(func(*testing.B))
-        if !ok {
-          tb.Fatalf("Found unexpected method signature for %s", methodName)
-        }
-      }
-    }
-  }
+			} else if strings.HasSuffix(methodName, "Test") {
+				_, ok := methodValue.Interface().(func(*testing.T))
+				if !ok {
+					tb.Fatalf(
+						"Unexpected method signature for %s. Expected func(*testing.T)",
+						methodName)
+				}
+			} else if strings.HasSuffix(methodName, "Benchmark") {
+				_, ok := methodValue.Interface().(func(*testing.B))
+				if !ok {
+					tb.Fatalf(
+						"Unexpected method signature for %s. Expected func(*testing.B)",
+						methodName)
+				}
+			}
+		}
+	}
 }
 
 // Suite's tests are of the form
-//    `func (testSuite) Test<Name>(*testing.T)`
+//
+//	`func (testSuite) Test<Name>(*testing.T)`
+//
 // RunTests will make use of SuiteSetup, TestSetup, TestCleanup, and
 // SuiteCleanup if they are defined.
 //
 // Note that the suite's name is not used for constructing the test cases'
 // names.
 func RunTests[SuiteT any](outerT *testing.T, suite SuiteT) {
-  outerT.Helper()
+	outerT.Helper()
 
-  checkSuiteSignatures(outerT, suite)
+	checkSuiteSignatures(outerT, suite)
 
-  var iSuite any = suite  // We can't type cast suite directly
+	var iSuite any = suite // We can't type cast suite directly
 
-  cleanupSuite, ok := iSuite.(SuiteCleanup)
-  if ok {
-    outerT.Cleanup(func() { cleanupSuite.CleanupSuite(outerT) })
-  }
+	cleanupSuite, ok := iSuite.(SuiteCleanup)
+	if ok {
+		outerT.Cleanup(func() { cleanupSuite.CleanupSuite(outerT) })
+	}
 
-  setupSuite, ok := iSuite.(SuiteSetup)
-  if ok {
-    setupSuite.SetupSuite(outerT)
-  }
+	setupSuite, ok := iSuite.(SuiteSetup)
+	if ok {
+		setupSuite.SetupSuite(outerT)
+	}
 
-  setupTest, ok := iSuite.(TestSetup)
-  if !ok {
-    setupTest = nil
-  }
+	setupTest, ok := iSuite.(TestSetup)
+	if !ok {
+		setupTest = nil
+	}
 
-  cleanupTest, ok := iSuite.(TestCleanup)
-  if !ok {
-    cleanupTest = nil
-  }
+	cleanupTest, ok := iSuite.(TestCleanup)
+	if !ok {
+		cleanupTest = nil
+	}
 
-  suiteValue := reflect.ValueOf(suite)
-  suiteType := reflect.TypeOf(suite)
+	suiteValue := reflect.ValueOf(suite)
+	suiteType := reflect.TypeOf(suite)
 
-  for i := 0; i < suiteValue.NumMethod(); i++ {
-    methodName := suiteType.Method(i).Name
+	for i := 0; i < suiteValue.NumMethod(); i++ {
+		methodName := suiteType.Method(i).Name
 
-    if !strings.HasPrefix(methodName, "Test") {
-      continue
-    }
+		if !strings.HasPrefix(methodName, "Test") {
+			continue
+		}
 
-    // suiteValue.Method instead of suiteType.Method to get the receiver-binded
-    // method.
-    methodValue := suiteValue.Method(i)
+		// suiteValue.Method instead of suiteType.Method to get the receiver-binded
+		// method.
+		methodValue := suiteValue.Method(i)
 
-    // We've already verify the signature
-    testFunc := methodValue.Interface().(func(*testing.T))
+		// We've already verify the signature
+		testFunc := methodValue.Interface().(func(*testing.T))
 
-    outerT.Run(
-      methodName[4:],  // trim "Test" prefix
-      func(innerT *testing.T) {
-        if cleanupTest != nil {
-          innerT.Cleanup(func() { cleanupTest.CleanupTest(innerT) })
-        }
+		outerT.Run(
+			methodName[4:], // trim "Test" prefix
+			func(innerT *testing.T) {
+				if cleanupTest != nil {
+					innerT.Cleanup(func() { cleanupTest.CleanupTest(innerT) })
+				}
 
-        if setupTest != nil {
-          setupTest.SetupTest(innerT)
-        }
+				if setupTest != nil {
+					setupTest.SetupTest(innerT)
+				}
 
-        testFunc(innerT)
-      })
-  }
+				testFunc(innerT)
+			})
+	}
 }
 
 // Suite's benchmarks are of the form
-//    `func (testSuite) Benchmark<Name>(*testing.B)`
+//
+//	`func (testSuite) Benchmark<Name>(*testing.B)`
+//
 // RunBenchmarks will make use of SuiteSetup, BenchmarkSetup,
 // BenchmarkCleanup, and SuiteCleanup if they are defined.
 //
 // Note that the suite's name is not used for constructing the benchmark cases'
 // names.
 func RunBenchmarks[SuiteT any](outerB *testing.B, suite SuiteT) {
-  outerB.Helper()
+	outerB.Helper()
 
-  outerB.StopTimer()
+	outerB.StopTimer()
 
-  checkSuiteSignatures(outerB, suite)
+	checkSuiteSignatures(outerB, suite)
 
-  var iSuite any = suite  // We can't type cast suite directly
+	var iSuite any = suite // We can't type cast suite directly
 
-  cleanupSuite, ok := iSuite.(SuiteCleanup)
-  if ok {
-    outerB.Cleanup(func() { cleanupSuite.CleanupSuite(outerB) })
-  }
+	cleanupSuite, ok := iSuite.(SuiteCleanup)
+	if ok {
+		outerB.Cleanup(func() { cleanupSuite.CleanupSuite(outerB) })
+	}
 
-  setupSuite, ok := iSuite.(SuiteSetup)
-  if ok {
-    setupSuite.SetupSuite(outerB)
-  }
+	setupSuite, ok := iSuite.(SuiteSetup)
+	if ok {
+		setupSuite.SetupSuite(outerB)
+	}
 
-  setupBenchmark, ok := iSuite.(BenchmarkSetup)
-  if !ok {
-    setupBenchmark = nil
-  }
+	setupBenchmark, ok := iSuite.(BenchmarkSetup)
+	if !ok {
+		setupBenchmark = nil
+	}
 
-  cleanupBenchmark, ok := iSuite.(BenchmarkCleanup)
-  if !ok {
-    cleanupBenchmark = nil
-  }
+	cleanupBenchmark, ok := iSuite.(BenchmarkCleanup)
+	if !ok {
+		cleanupBenchmark = nil
+	}
 
-  suiteValue := reflect.ValueOf(suite)
-  suiteType := reflect.TypeOf(suite)
+	suiteValue := reflect.ValueOf(suite)
+	suiteType := reflect.TypeOf(suite)
 
-  for i := 0; i < suiteValue.NumMethod(); i++ {
-    methodName := suiteType.Method(i).Name
+	for i := 0; i < suiteValue.NumMethod(); i++ {
+		methodName := suiteType.Method(i).Name
 
-    if !strings.HasPrefix(methodName, "Benchmark") {
-      continue
-    }
+		if !strings.HasPrefix(methodName, "Benchmark") {
+			continue
+		}
 
-    // suiteValue.Method instead of suiteType.Method to get the receiver-binded
-    // method.
-    methodValue := suiteValue.Method(i)
+		// suiteValue.Method instead of suiteType.Method to get the receiver-binded
+		// method.
+		methodValue := suiteValue.Method(i)
 
-    // We've already verify the signature
-    benchmarkFunc := methodValue.Interface().(func(*testing.B))
+		// We've already verify the signature
+		benchmarkFunc := methodValue.Interface().(func(*testing.B))
 
-    outerB.Run(
-      methodName[9:],  // trim "Benchmark" prefix
-      func(innerB *testing.B) {
-        innerB.StopTimer()
+		outerB.Run(
+			methodName[9:], // trim "Benchmark" prefix
+			func(innerB *testing.B) {
+				innerB.StopTimer()
 
-        if cleanupBenchmark != nil {
-          innerB.Cleanup(func() { cleanupBenchmark.CleanupBenchmark(innerB) })
-        }
+				if cleanupBenchmark != nil {
+					innerB.Cleanup(func() { cleanupBenchmark.CleanupBenchmark(innerB) })
+				}
 
+				if setupBenchmark != nil {
+					setupBenchmark.SetupBenchmark(innerB)
+				}
 
-        if setupBenchmark != nil {
-          setupBenchmark.SetupBenchmark(innerB)
-        }
-
-        innerB.StartTimer()
-        benchmarkFunc(innerB)
-        innerB.StopTimer()
-      })
-  }
+				innerB.StartTimer()
+				benchmarkFunc(innerB)
+				innerB.StopTimer()
+			})
+	}
 }
--- go/testing/suite/suite_test.go
+++ go/testing/suite/suite_test.go
@@ -1,184 +1,184 @@
 package suite_test
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/pattyshack/bt/go/testing/expect"
-  "github.com/pattyshack/bt/go/testing/suite"
+	"github.com/pattyshack/bt/go/testing/expect"
+	"github.com/pattyshack/bt/go/testing/suite"
 )
 
 // Note: suite's struct name is not included in the test's full name.
 type Suite struct {
-  setupSuite map[string]bool
-  cleanupSuite map[string]bool
+	setupSuite   map[string]bool
+	cleanupSuite map[string]bool
 
-  setupTests map[string]bool
-  tests map[string]bool
-  cleanupTests map[string]bool
+	setupTests   map[string]bool
+	tests        map[string]bool
+	cleanupTests map[string]bool
 
-  failTestSetup bool
-  failTest bool
-  failTestCleanup bool
+	failTestSetup   bool
+	failTest        bool
+	failTestCleanup bool
 
-  setupBenchmarks map[string]int
-  benchmarks map[string]int
-  cleanupBenchmarks map[string]int
+	setupBenchmarks   map[string]int
+	benchmarks        map[string]int
+	cleanupBenchmarks map[string]int
 
-  failBenchmarkSetup bool
-  failBenchmark bool
-  failBenchmarkCleanup bool
+	failBenchmarkSetup   bool
+	failBenchmark        bool
+	failBenchmarkCleanup bool
 }
 
 func newSuite(
-  failTestSetup bool,
-  failTest bool,
-  failTestCleanup bool,
-  failBenchmarkSetup bool,
-  failBenchmark bool,
-  failBenchmarkCleanup bool,
+	failTestSetup bool,
+	failTest bool,
+	failTestCleanup bool,
+	failBenchmarkSetup bool,
+	failBenchmark bool,
+	failBenchmarkCleanup bool,
 ) *Suite {
-  return &Suite{
-    setupSuite: map[string]bool{},
-    cleanupSuite: map[string]bool{},
+	return &Suite{
+		setupSuite:   map[string]bool{},
+		cleanupSuite: map[string]bool{},
 
-    setupTests: map[string]bool{},
-    tests: map[string]bool{},
-    cleanupTests: map[string]bool{},
+		setupTests:   map[string]bool{},
+		tests:        map[string]bool{},
+		cleanupTests: map[string]bool{},
 
-    failTestSetup: failTestSetup,
-    failTest: failTest,
-    failTestCleanup: failTestCleanup,
+		failTestSetup:   failTestSetup,
+		failTest:        failTest,
+		failTestCleanup: failTestCleanup,
 
-    setupBenchmarks: map[string]int{},
-    benchmarks: map[string]int{},
-    cleanupBenchmarks: map[string]int{},
+		setupBenchmarks:   map[string]int{},
+		benchmarks:        map[string]int{},
+		cleanupBenchmarks: map[string]int{},
 
-    failBenchmarkSetup: failBenchmarkSetup,
-    failBenchmark: failBenchmark,
-    failBenchmarkCleanup: failBenchmarkCleanup,
-  }
+		failBenchmarkSetup:   failBenchmarkSetup,
+		failBenchmark:        failBenchmark,
+		failBenchmarkCleanup: failBenchmarkCleanup,
+	}
 }
 
 func (s *Suite) SetupSuite(tb testing.TB) {
-  expect.Equal(tb, 0, len(s.setupSuite))
-  expect.Equal(tb, 0, len(s.setupTests))
-  expect.Equal(tb, 0, len(s.setupBenchmarks))
+	expect.Equal(tb, 0, len(s.setupSuite))
+	expect.Equal(tb, 0, len(s.setupTests))
+	expect.Equal(tb, 0, len(s.setupBenchmarks))
 
-  expect.Equal(tb, 0, len(s.tests))
-  expect.Equal(tb, 0, len(s.benchmarks))
+	expect.Equal(tb, 0, len(s.tests))
+	expect.Equal(tb, 0, len(s.benchmarks))
 
-  expect.Equal(tb, 0, len(s.cleanupSuite))
-  expect.Equal(tb, 0, len(s.cleanupTests))
-  expect.Equal(tb, 0, len(s.cleanupBenchmarks))
+	expect.Equal(tb, 0, len(s.cleanupSuite))
+	expect.Equal(tb, 0, len(s.cleanupTests))
+	expect.Equal(tb, 0, len(s.cleanupBenchmarks))
 
-  s.setupSuite[tb.Name()] = true
+	s.setupSuite[tb.Name()] = true
 }
 
 func (s *Suite) CleanupSuite(tb testing.TB) {
-  expect.Equal(tb, 1, len(s.setupSuite))
-  expect.Equal(tb, 0, len(s.cleanupSuite))
+	expect.Equal(tb, 1, len(s.setupSuite))
+	expect.Equal(tb, 0, len(s.cleanupSuite))
 
-  expect.True(tb, s.setupSuite[tb.Name()])
-  expect.False(tb, s.cleanupSuite[tb.Name()])
+	expect.True(tb, s.setupSuite[tb.Name()])
+	expect.False(tb, s.cleanupSuite[tb.Name()])
 
-  s.cleanupSuite[tb.Name()] = true
+	s.cleanupSuite[tb.Name()] = true
 
-  // Note: we can't verify if the test suite had cleanup while still inside the
-  // test.  The best we can do is panic in CleanupSuite to see if it ran or not.
-  // panic("GOT HERE")
+	// Note: we can't verify if the test suite had cleanup while still inside the
+	// test.  The best we can do is panic in CleanupSuite to see if it ran or not.
+	// panic("GOT HERE")
 }
 
 func (s *Suite) SetupTest(t *testing.T) {
-  expect.Equal(t, 1, len(s.setupSuite))
-  expect.Equal(t, 0, len(s.cleanupSuite))
+	expect.Equal(t, 1, len(s.setupSuite))
+	expect.Equal(t, 0, len(s.cleanupSuite))
 
-  expect.False(t, s.setupTests[t.Name()])
-  expect.False(t, s.tests[t.Name()])
-  expect.False(t, s.cleanupTests[t.Name()])
+	expect.False(t, s.setupTests[t.Name()])
+	expect.False(t, s.tests[t.Name()])
+	expect.False(t, s.cleanupTests[t.Name()])
 
-  s.setupTests[t.Name()] = true
+	s.setupTests[t.Name()] = true
 
-  expect.False(t, s.failTestSetup)
+	expect.False(t, s.failTestSetup)
 }
 
 func (s *Suite) CleanupTest(t *testing.T) {
-  expect.Equal(t, 1, len(s.setupSuite))
-  expect.Equal(t, 0, len(s.cleanupSuite))
+	expect.Equal(t, 1, len(s.setupSuite))
+	expect.Equal(t, 0, len(s.cleanupSuite))
 
-  expect.True(t, s.setupTests[t.Name()])
-  expect.True(t, s.tests[t.Name()])
-  expect.False(t, s.cleanupTests[t.Name()])
+	expect.True(t, s.setupTests[t.Name()])
+	expect.True(t, s.tests[t.Name()])
+	expect.False(t, s.cleanupTests[t.Name()])
 
-  s.cleanupTests[t.Name()] = true
+	s.cleanupTests[t.Name()] = true
 
-  expect.False(t, s.failTestCleanup)
+	expect.False(t, s.failTestCleanup)
 }
 
 func (s *Suite) test(t *testing.T) {
-  expect.Equal(t, 1, len(s.setupSuite))
-  expect.Equal(t, 0, len(s.cleanupSuite))
+	expect.Equal(t, 1, len(s.setupSuite))
+	expect.Equal(t, 0, len(s.cleanupSuite))
 
-  expect.True(t, s.setupTests[t.Name()])
-  expect.False(t, s.tests[t.Name()])
-  expect.False(t, s.cleanupTests[t.Name()])
+	expect.True(t, s.setupTests[t.Name()])
+	expect.False(t, s.tests[t.Name()])
+	expect.False(t, s.cleanupTests[t.Name()])
 
-  s.tests[t.Name()] = true
+	s.tests[t.Name()] = true
 
-  expect.False(t, s.failTest)
+	expect.False(t, s.failTest)
 }
 
 func (s *Suite) SetupBenchmark(b *testing.B) {
-  expect.Equal(b, 1, len(s.setupSuite))
-  expect.Equal(b, 0, len(s.cleanupSuite))
+	expect.Equal(b, 1, len(s.setupSuite))
+	expect.Equal(b, 0, len(s.cleanupSuite))
 
-  s.setupBenchmarks[b.Name()]++
+	s.setupBenchmarks[b.Name()]++
 
-  currentRun := s.setupBenchmarks[b.Name()]
+	currentRun := s.setupBenchmarks[b.Name()]
 
-  expect.Equal(b, currentRun - 1, s.benchmarks[b.Name()])
-  expect.Equal(b, currentRun - 1, s.cleanupBenchmarks[b.Name()])
+	expect.Equal(b, currentRun-1, s.benchmarks[b.Name()])
+	expect.Equal(b, currentRun-1, s.cleanupBenchmarks[b.Name()])
 
-  expect.False(b, s.failBenchmarkSetup)
+	expect.False(b, s.failBenchmarkSetup)
 }
 
 func (s *Suite) CleanupBenchmark(b *testing.B) {
-  expect.Equal(b, 1, len(s.setupSuite))
-  expect.Equal(b, 0, len(s.cleanupSuite))
+	expect.Equal(b, 1, len(s.setupSuite))
+	expect.Equal(b, 0, len(s.cleanupSuite))
 
-  currentRun := s.setupBenchmarks[b.Name()]
+	currentRun := s.setupBenchmarks[b.Name()]
 
-  s.cleanupBenchmarks[b.Name()]++
+	s.cleanupBenchmarks[b.Name()]++
 
-  expect.Equal(b, currentRun, s.benchmarks[b.Name()])
-  expect.Equal(b, currentRun, s.cleanupBenchmarks[b.Name()])
+	expect.Equal(b, currentRun, s.benchmarks[b.Name()])
+	expect.Equal(b, currentRun, s.cleanupBenchmarks[b.Name()])
 
-  expect.False(b, s.failBenchmarkCleanup)
+	expect.False(b, s.failBenchmarkCleanup)
 }
 
 func (s *Suite) benchmark(b *testing.B) {
-  expect.Equal(b, 1, len(s.setupSuite))
-  expect.Equal(b, 0, len(s.cleanupSuite))
+	expect.Equal(b, 1, len(s.setupSuite))
+	expect.Equal(b, 0, len(s.cleanupSuite))
 
-  currentRun := s.setupBenchmarks[b.Name()]
+	currentRun := s.setupBenchmarks[b.Name()]
 
-  s.benchmarks[b.Name()]++
+	s.benchmarks[b.Name()]++
 
-  expect.Equal(b, currentRun, s.benchmarks[b.Name()])
-  expect.Equal(b, currentRun - 1, s.cleanupBenchmarks[b.Name()])
+	expect.Equal(b, currentRun, s.benchmarks[b.Name()])
+	expect.Equal(b, currentRun-1, s.cleanupBenchmarks[b.Name()])
 
-  expect.False(b, s.failBenchmark)
+	expect.False(b, s.failBenchmark)
 }
 
 func (s *Suite) TestUnittestCase1(t *testing.T) {
-  s.test(t)
+	s.test(t)
 }
 
 func (s *Suite) TestUnittestCase2(t *testing.T) {
-  s.test(t)
+	s.test(t)
 }
 
 func (s *Suite) TestUnittestCase3(t *testing.T) {
-  s.test(t)
+	s.test(t)
 }
 
 /*
@@ -189,15 +189,15 @@ func (s *Suite) TestBadSignature(t *testing.T, _ int) {
 */
 
 func (s *Suite) BenchmarkBenchCase1(b *testing.B) {
-  s.benchmark(b)
+	s.benchmark(b)
 }
 
 func (s *Suite) BenchmarkBenchCase2(b *testing.B) {
-  s.benchmark(b)
+	s.benchmark(b)
 }
 
 func (s *Suite) BenchmarkBenchCase3(b *testing.B) {
-  s.benchmark(b)
+	s.benchmark(b)
 }
 
 /*
@@ -208,48 +208,48 @@ func (s *Suite) BenchmarkBadSignature(b *testing.B, _ int) {
 */
 
 func TestSuite(t *testing.T) {
-  // In general, tests should just create the suite and call suite.RunTests().
-  tests := newSuite(false, false, false, false, false, false)
-  suite.RunTests(t, tests)
+	// In general, tests should just create the suite and call suite.RunTests().
+	tests := newSuite(false, false, false, false, false, false)
+	suite.RunTests(t, tests)
 
-  // The following are additional checks to verifies suite internal
-  // implementation, and should not be parts of real tests.
+	// The following are additional checks to verifies suite internal
+	// implementation, and should not be parts of real tests.
 
-  expectedTestCases := map[string]bool {
-    "TestSuite/UnittestCase1": true,
-    "TestSuite/UnittestCase2": true,
-    "TestSuite/UnittestCase3": true,
-  }
+	expectedTestCases := map[string]bool{
+		"TestSuite/UnittestCase1": true,
+		"TestSuite/UnittestCase2": true,
+		"TestSuite/UnittestCase3": true,
+	}
 
-  expect.Equal(t, expectedTestCases, tests.setupTests)
-  expect.Equal(t, expectedTestCases, tests.tests)
-  expect.Equal(t, expectedTestCases, tests.cleanupTests)
+	expect.Equal(t, expectedTestCases, tests.setupTests)
+	expect.Equal(t, expectedTestCases, tests.tests)
+	expect.Equal(t, expectedTestCases, tests.cleanupTests)
 
-  expectedSuite := map[string]bool { "TestSuite": true }
+	expectedSuite := map[string]bool{"TestSuite": true}
 
-  expect.Equal(t, expectedSuite, tests.setupSuite)
+	expect.Equal(t, expectedSuite, tests.setupSuite)
 
-  // Note: we can't verify if the test suite had cleanup while still inside the
-  // test.  The best we can do is panic in CleanupSuite to see if it ran or not.
+	// Note: we can't verify if the test suite had cleanup while still inside the
+	// test.  The best we can do is panic in CleanupSuite to see if it ran or not.
 }
 
 func BenchmarkSuite(b *testing.B) {
-  // In general, benchmarks should just create the suite and call
-  // suite.RunBenchmarks().
-  benchmarks := newSuite(false, false, false, false, false, false)
-  suite.RunBenchmarks(b, benchmarks)
+	// In general, benchmarks should just create the suite and call
+	// suite.RunBenchmarks().
+	benchmarks := newSuite(false, false, false, false, false, false)
+	suite.RunBenchmarks(b, benchmarks)
 
-  // The following are additional checks to verifies suite internal
-  // implementation, and should not be parts of real benchmarks.
+	// The following are additional checks to verifies suite internal
+	// implementation, and should not be parts of real benchmarks.
 
-  expect.Equal(b, 3, len(benchmarks.setupBenchmarks))
-  expect.Equal(b, benchmarks.setupBenchmarks, benchmarks.benchmarks)
-  expect.Equal(b, benchmarks.setupBenchmarks, benchmarks.cleanupBenchmarks)
+	expect.Equal(b, 3, len(benchmarks.setupBenchmarks))
+	expect.Equal(b, benchmarks.setupBenchmarks, benchmarks.benchmarks)
+	expect.Equal(b, benchmarks.setupBenchmarks, benchmarks.cleanupBenchmarks)
 
-  expectedSuite := map[string]bool { "BenchmarkSuite": true }
+	expectedSuite := map[string]bool{"BenchmarkSuite": true}
 
-  expect.Equal(b, expectedSuite, benchmarks.setupSuite)
+	expect.Equal(b, expectedSuite, benchmarks.setupSuite)
 
-  // Note: we can't verify if the test suite had cleanup while still inside the
-  // test.  The best we can do is panic in CleanupSuite to see if it ran or not.
+	// Note: we can't verify if the test suite had cleanup while still inside the
+	// test.  The best we can do is panic in CleanupSuite to see if it ran or not.
 }
